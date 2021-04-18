2895e9ba6357500ddcfe285a85097ce535897360
dimash200101 <71224229+dimash200101@users.noreply.github.com>
2021-04-18 17:50:12 +0600

    Add files via upload

---
+++ Dice Roll App Project/Dice Roll App/Dice Roll App/ViewController.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        let firstNumber = arc4random_uniform(5) + 1
+        let secondNumber = arc4random_uniform(5) + 1
+        
+        label.text = "The sum is: \(firstNumber + secondNumber)"
+        
+        leftImageView.image = UIImage(named: "Dice\(firstNumber)")
+        
+        rightImageView.image = UIImage(named: "Dice\(secondNumber)")
+        
+    }
+    
+    
+}
+
